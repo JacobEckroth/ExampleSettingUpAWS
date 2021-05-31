7a753232ff4b781c273a9439c164bc4dab948a52
Jacob Eckroth <57682176+JacobEckroth@users.noreply.github.com>
2021-05-31 08:56:47 -0700

    Update server.js

--- server.js
+++ server.js
@@ -3,9 +3,11 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+    console.log("got a request");
+    res.send('Hello World!')
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
+
+    console.log(`Example app listening at  port 300`)
 })
